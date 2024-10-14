61c81a8070b7661bb6996d7fcbc01677dde6b705
Abu Hanif <ahanif2500@gmail.com>
2024-10-14 21:08:53 +0600

    basic statistics math program implented

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
-#include "exercise/pascalTriangle.c"
+#include "exercise/basic_statistics.c"
 
 int main() {
-    printPascalTriangle(5);
+    int size;
+    printf("Enter size of array: ");
+    scanf("%d", &size);
+    int array[size];
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &array[i]);
+    }
+    staticWizard(array, size);
     return 0;
 }
